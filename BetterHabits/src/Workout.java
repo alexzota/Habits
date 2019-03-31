98fb0f2577cb53eefcb98f9df3c70debbf199a62
alexzota <40060185+alexzota@users.noreply.github.com>
2019-04-01 01:19:43 +0300

    Update Workout.java

--- BetterHabits/src/Workout.java
+++ BetterHabits/src/Workout.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Workout {
+    private int day;
+    private ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+    public void print(){
+        System.out.println("DAY " + day);
+        for(Exercise idx : exercises) idx.print();
+    }
 }
